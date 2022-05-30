ae4e18f58a8f1c16495cc784e23139e8928f5b46
Mateusz Przybysz <38528098+maprzybysz@users.noreply.github.com>
2022-05-30 19:46:35 +0200

    Update Root.js

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
